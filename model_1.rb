5d295a97bd81ef99721de6d626d2ba325e01956f
Tanner Dale <tanner@tannerdale.com>
2021-04-15 20:55:52 -0500

    Add hash variable

--- model_1.rb
+++ model_1.rb
@@ -6,3 +6,11 @@ height = 6.1
 weight = 155
 
 favorite_colors = ["black", "blue", "red"]
+
+recent_email = {
+  sender: "github.com",
+  subject: "Updated policies",
+  time: "7:00 pm",
+  date: "Apr 14",
+  subject: "Blah bl"
+}
